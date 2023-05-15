08e201d33dd50564e52c905c4c4cdbf0fa711411
Daniel <DanielCarrete_Dev@outlook.com>
2023-05-15 00:36:25 -0500

    writing

--- plots.py
+++ plots.py
@@ -2,16 +2,24 @@ from manim import *
 import numpy as np
 
 
+mu = complex(0.2, 0.15)
+u0 = 100
+gamma = 4*np.pi*100*np.sqrt((mu.real - mu.imag)**2+(mu.imag)**2) * \
+    (mu.imag/np.sqrt((mu.real - mu.imag)**2+(mu.imag)**2))
+a = 1.2
+R = 1
+
+
 def circle(theta):
-    return np.exp(complex(0, PI*theta))
+    return np.exp(complex(0, np.pi*theta))
 
 
 def shiftfunction(function):
-    return complex(0.2, 0.15) - function
+    return mu - function
 
 
 def joukouski(z):
-    return z + ((1.2)**2)/z
+    return z + ((a)**2)/z
 
 
 class JoukouskiTransform(Scene):
@@ -53,7 +61,7 @@ class JoukouskiTransform(Scene):
                     0,
                 ]
             ),
-            t_range=[0, 2 * PI],
+            t_range=[0, 2 * np.pi],
             color="#0FF1CE",
         )
 
@@ -65,7 +73,7 @@ class JoukouskiTransform(Scene):
                     0,
                 ]
             ),
-            t_range=[0, 2 * PI],
+            t_range=[0, 2 * np.pi],
             color="#0FF1CE",
         )
 
@@ -77,7 +85,7 @@ class JoukouskiTransform(Scene):
                     0,
                 ]
             ),
-            t_range=[0, 2 * PI],
+            t_range=[0, 2 * np.pi],
             color="#0FF1CE",
         )
 
@@ -96,7 +104,7 @@ class Airfoil(Scene):
                     0,
                 ]
             ),
-            t_range=[0, 2 * PI],
+            t_range=[0, 2 * np.pi],
             color="#0FF1CE",
         )
         self.add(plane, curve)
@@ -105,7 +113,6 @@ class Airfoil(Scene):
 class uniformFlow(Scene):
     def construct(self):
         def func(pos): return ((80*RIGHT))
-
         self.add(ArrowVectorField(func))
 
 
@@ -135,14 +142,45 @@ class FlowAroundCircle(Scene):
 class StreamLinesCircle(Scene):
     def construct(self):
         def func(pos):
+            div = 200
             if np.abs(pos[0]) < 1 and np.abs(pos[1]) < 1:
                 return 0*UR
             else:
-                return (200*pos[1]**2/(pos[0]**2 + pos[1]**2)**2 + 100 - 100/(pos[0]**2 + pos[1]**2))*RIGHT + (-200*pos[0]*pos[1]/(pos[0]**2 + pos[1]**2)**2)*UP
+                return ((200*pos[1]**2/(pos[0]**2 + pos[1]**2)**2 + 100 - 100/(pos[0]**2 + pos[1]**2))*RIGHT + (-200*pos[0]*pos[1]/(pos[0]**2 + pos[1]**2)**2)*UP)/div
+        vf = ArrowVectorField(func)
+        plane = NumberPlane()
+        self.add(vf, plane, StreamLines(
+            func, stroke_width=3, max_anchors_per_line=30))
 
-        self.add(StreamLines(func))
+
+class StreamLinesCircleCirculation(Scene):
+    def construct(self):
+        def func(pos):
+            div = 200
+            if np.abs(pos[0]) < 1 and np.abs(pos[1]) < 1:
+                return 0*UR
+            else:
+                return ((-np.pi*gamma*pos[1]/(2*(pos[0]**2 + pos[1]**2)) + 2*u0*pos[1]**2/(pos[0]**2 + pos[1]**2)**2 + u0*(1 - 1/(pos[0]**2 + pos[1]**2)))*RIGHT + (np.pi*gamma*pos[0]/(2*(pos[0]**2 + pos[1]**2)) - 2*u0*pos[0]*pos[1]/(pos[0]**2 + pos[1]**2)**2)*UP)/div
+        vf = ArrowVectorField(func)
+        plane = NumberPlane()
+        self.add(vf, plane, StreamLines(
+            func, stroke_width=3, max_anchors_per_line=30))
+
+
+class StreamLinesAirfoil(Scene):
+    def construct(self):
+        def func1(pos):
+            if np.abs(pos[0]) < 1 and np.abs(pos[1]) < 1:
+                return 0*UR
+            else:
+                return ((gamma*pos[1]/(2*np.pi*((pos[0])**2 + (pos[1])**2)) + 2*u0*pos[1]**2/((pos[0])**2 + (pos[1])**2)**2 - u0 - u0/((pos[0])**2 + (pos[1])**2) - (-gamma*pos[1]/(2*np.pi*((pos[0])**2 + (pos[1])**2)) + 2*u0*pos[1]*pos[1]/((pos[0])**2 + (pos[1])**2)**2)*(a**2/(complex(0, 1)*gamma*np.log(complex(pos[0], pos[1]))/(2*np.pi) + mu - u0*(complex(pos[0], pos[1])) - u0/(complex(pos[0], pos[1])))**2).imag + (-gamma*pos[1]/(2*np.pi*((pos[0])**2 + (pos[1])**2)) - u0*(pos[1]**2/((pos[0])**2 + (pos[1])**2)**2 - pos[1]**2/((pos[0])**2 + (pos[1])**2)**2) + u0)*(a**2/(complex(0, 1)*gamma*np.log(complex(pos[0], pos[1]))/(2*np.pi) + mu - u0*(complex(pos[0], pos[1])) - u0/(complex(pos[0], pos[1])))**2).real)*RIGHT + (-gamma*pos[1]/(2*np.pi*((pos[0])**2 + (pos[1])**2)) + 2*u0*pos[1]*pos[1]/((pos[0])**2 + (pos[1])**2)**2 + (gamma*pos[1]/(2*np.pi*((pos[0])**2 + (pos[1])**2)) - 2*u0*pos[1]*pos[1]/((pos[0])**2 + (pos[1])**2)**2)*(a**2/(complex(0, 1)*gamma*np.log(complex(pos[0], pos[1]))/(2*np.pi) + mu - u0*(complex(pos[0], pos[1])) - u0/(complex(pos[0], pos[1])))**2).real - (-gamma*pos[1]/(2*np.pi*((pos[0])**2 + (pos[1])**2)) - u0*(pos[1]**2/((pos[0])**2 + (pos[1])**2)**2 - pos[1]**2/((pos[0])**2 + (pos[1])**2)**2) + u0)*(a**2/(complex(0, 1)*gamma*np.log(complex(pos[0], pos[1]))/(2*np.pi) + mu - u0*(complex(pos[0], pos[1])) - u0/(complex(pos[0], pos[1])))**2).imag)*UP)/200
+
+        vf = ArrowVectorField(func1)
+        plane = NumberPlane()
+        self.add(vf, plane, StreamLines(
+            func1, stroke_width=3, max_anchors_per_line=30))
 
 
 with tempconfig({"quality": "medium_quality", "preview": False, "pixel_width": 1920, "pixel_height": 1080}):
-    scene = StreamLinesCircle()
+    scene = StreamLinesAirfoil()
     scene.render()
